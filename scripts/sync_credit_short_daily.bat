cf86972044af23b422aab4e0bfad7cfc174d5dd9
dragonyongs <dragonyongs@naver.com>
2026-06-08 18:08:23 +0900

    feat: sync_credit_short_daily.bat 파일 삭제

--- scripts/sync_credit_short_daily.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-chcp 65001 >nul
-setlocal
-cd /d "D:\Work\dev\github\signal-scanner-bot"
-set PYTHONIOENCODING=utf-8
-"D:\Work\dev\github\signal-scanner-bot\.venv\Scripts\python.exe" "D:\Work\dev\github\signal-scanner-bot\scripts\update_credit_short.py" >> "D:\Work\dev\github\signal-scanner-bot\logs\credit_short_daily.log" 2>&1
-set EXITCODE=%ERRORLEVEL%
-endlocal & exit /b %EXITCODE%
